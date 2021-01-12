a1e1be6b240f7e52b602c8349bbe906a0690f22e
Andrej Snitko <snitko@seznam.cz>
2021-01-12 16:45:18 +0100

    initial commit

---
+++ bitkonj.py
@@ -0,0 +1,109 @@
+import os
+import logging
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+import aiohttp
+import requests
+import json
+from util import db
+
+
+logging.basicConfig(level=logging.INFO)
+
+TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_API_SECRET = os.getenv("BINANCE_API_SECRET")
+BINANCE_BASEURL = 'https://api.binance.com'
+
+headers = {
+    'X-MBX-APIKEY': BINANCE_API_KEY
+}
+
+bot = Bot(token=TELEGRAM_API_TOKEN)
+dp = Dispatcher(bot)
+last_action = 'BUY'
+last_buy = 1000
+last_sell = 100000
+btc_balance = 0.008
+usd_balance = 0
+
+
+def auth(func):
+
+    async def wrapper(message):
+        if message['from']['id'] != TELEGRAM_USER_ID:
+            return await message.reply("Access Denied", reply=False)
+        return await func(message)
+
+    return wrapper
+
+@dp.message_handler(commands=['start', 'help'])
+@auth
+async def send_welcome(message: types.Message):
+    await message.reply("Starting...")
+
+async def report():
+    global last_action, last_buy, last_sell, btc_balance, usd_balance
+
+    # GET THE CURRENT PRICE FOR BITCOIN
+    params = {
+        'symbol': 'BTCUSDT'
+    }
+    response = requests.get(BINANCE_BASEURL + '/api/v3/ticker/price',
+                            headers=headers,
+                            params=params)
+    if response.status_code == 200:
+        price = round(float(response.json()['price']))
+    else:
+        await bot.send_message(
+            TELEGRAM_CHAT_ID,
+            f"Код {response.status_code}! Сматываю удочки! ПЕРЕЗАПУСТИ МЕНЯ, БРАТАН!"
+        )
+        raise Exception(f"{response.status_code} error!")
+
+    if last_buy == 0:
+        last_buy = price
+    if last_sell == 0:
+        last_sell = price
+    price, coin, action, average_price = db.create(price, last_buy, last_sell, last_action)
+
+    if action == 'BUY' and last_action != 'BUY':
+        last_action = 'BUY'
+        last_buy = price
+        btc_balance = usd_balance / price
+        usd_balance = 0
+        await bot.send_message(
+            TELEGRAM_CHAT_ID,
+            f"{price} BOUGHT ------------------------------------------------",
+            disable_notification=True
+        )
+
+    if action == 'SELL' and last_action != 'SELL':
+        last_action = 'SELL'
+        last_sell = price
+        usd_balance = btc_balance * price
+        btc_balance = 0
+        await bot.send_message(
+            TELEGRAM_CHAT_ID,
+            f"{price} SOLD -------------------------------------------------",
+            disable_notification=True
+        )
+
+    await bot.send_message(
+        TELEGRAM_CHAT_ID,
+        f"{price} --- {average_price} --- {btc_balance} --- {usd_balance}",
+        disable_notification=True
+    )
+
+async def run():
+    while True:
+        await report()
+        await asyncio.sleep(5)
+
+
+if __name__ == '__main__':
+    # executor.start_polling(dp, skip_updates=True)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
---
+++ util/db.py
@@ -0,0 +1,86 @@
+import os
+import sys
+import datetime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy_utils import ChoiceType
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Tick(Base):
+    COIN_TYPES = [
+        ('btc', 'BTC'),
+        ('eth', 'ETH')
+    ]
+
+    DIR_TYPES = [
+        ('rise', 'rise'),
+        ('drop', 'drop'),
+        ('same', 'same'),
+    ]
+
+    __tablename__ = 'tick'
+    id = Column(Integer, primary_key=True)
+    price = Column(Integer, nullable=False)
+    coin = Column(ChoiceType(COIN_TYPES), nullable=False)
+    timestamp = Column(DateTime, default=datetime.datetime.utcnow)
+    direction = Column(ChoiceType(DIR_TYPES), nullable=False)
+
+engine = create_engine('sqlite:///bitkonj.db')
+Base.metadata.bind = engine
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def create(price, last_buy, last_sell, last_action, coin='btc'):
+    action = 'SIT'
+
+    try:
+        last_ticks100 = session.query(Tick).all()[-101:-1]
+        total = 0
+        for i in last_ticks100:
+            total += i.price
+        average_price = total / 100
+    except Exception as e:
+        average_price = price
+
+    try:
+        last_tick = session.query(Tick).all()[-1]
+        if last_tick.price > price:
+            direction = 'drop'
+        elif last_tick.price < price:
+            direction = 'rise'
+        else:
+            direction = 'same'
+    except Exception as e:
+        direction = 'same'
+
+    new_tick = Tick(price=price, coin=coin, direction=direction)
+    session.add(new_tick)
+    session.commit()
+
+    if last_action == 'BUY' and abs(last_buy - price) < 100:
+        return price, coin, action, average_price
+
+    if last_action == 'SELL' and abs(last_sell - price) < 100:
+        return price, coin, action, average_price
+
+    try:
+        if last_tick.direction == 'drop' and direction == 'rise':
+            if last_sell > price:
+                action = 'BUY'
+            else:
+                if price - last_sell > 500:
+                    action = 'BUY'
+        elif last_tick.direction == 'rise' and direction == 'drop':
+            if last_buy < price:
+                action = 'SELL'
+            else:
+                if last_buy - price > 500:
+                    action = 'SELL'
+    except Exception:
+        pass
+
+    return price, coin, action, average_price
